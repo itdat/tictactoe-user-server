9a5e303bcb5cbe204ada9ce8e476d1e3aef26142
Van Huy <1712056@student.hcmus.edu.vn>
2020-12-09 17:16:58 +0700

    Add CORS and RESTful API, fix login validation

--- routes/index.js
+++ routes/index.js
@@ -1,119 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
 const passport = require("passport");
-const validateRegisterInput = require("../validation/register");
-const validateLoginInput = require("../validation/login");
-//Register
-router.post("/register", async (req, res) => {
-  const { errors, isValid } = await validateRegisterInput(req.body);
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  const { name, username, password } = req.body;
-  try {
-    let user = await User.findOne({ username });
-    if (user) {
-      return res.status(400).json({ msg: "Username already exists" });
-    } else {
-      const newUser = new User({
-        name: name,
-        username: username,
-        password: password,
-      });
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) console.error("There was an error", err);
-        else {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) console.error("There was an error", err);
-            else {
-              newUser.password = hash;
-              newUser.save().then((user) => {
-                res.status(200).json(user);
-              });
-            }
-          });
-        }
-      });
-    }
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
+const userController = require("./../controllers/userController");
 
-//Login
-router.post("/login", async (req, res) => {
-  const { errors, isValid } = await validateLoginInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const username = req.body.username;
-  const password = req.body.password;
-  try {
-    let user = await User.findOne({ username });
-    if (!user) {
-      errors.email = "User not found";
-      return res.status(404).json(errors);
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Incorrect password" });
-    }
-    const payload = {
-      id: user.id,
-      name: user.name,
-    };
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      {
-        expiresIn: 3600,
-      },
-      (err, token) => {
-        if (err) console.error("There is some error in token", err);
-        else {
-          res.json({
-            success: true,
-            token: `Bearer ${token}`,
-          });
-        }
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-//Login with Facebook
-router.get("/login/facebook", passport.authenticate("facebook"));
-
-router.get(
-  "/login/facebook/callback",
+router.route("/register").post(userController.Register);
+router.route("/login").post(userController.Login);
+//Login with facebook
+router.route("/login/facebook").get(passport.authenticate("facebook"));
+router.route("/login/facebook/callback").get(
   passport.authenticate("facebook", {
     successRedirect: "/",
-    failureRedirect: "/fail",
+    failureRedirect: "https://tictactoe-user.netlify.app/login",
   })
 );
-
-// Login with Google
-router.get(
-  "/login/google",
+//Login with google
+router.route("/login/google").get(
   passport.authenticate("google", {
     scope: ["profile"],
   })
 );
-
-router.get(
-  "/login/google/callback",
+router.route("/login/google/callback").get(
   passport.authenticate("google", {
     successRedirect: "/",
-    failureRedirect: "/login",
+    failureRedirect: "https://tictactoe-user.netlify.app/login",
   })
 );
 module.exports = router;
--- server.js
+++ server.js
@@ -20,6 +20,15 @@ connectDB();
 
 // Init middleware
 app.use(express.json());
+//CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+
+  next();
+})
 app.use(passport.initialize());
 //Define routes
 app.use("/users", require("./routes/index"));
@@ -63,7 +72,4 @@ io.on('connect', (socket) => {
 });
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}...`);
-// });
+
